bc23a7ae79814be382288ed35443a488b78daf2c
bealid <beate.lidstrom@gmail.com>
2021-11-25 09:43:01 +0100

    Init LDA QDA

---
+++ classificationLDA.py
@@ -0,0 +1,11 @@
+# Classification LDA and QDA
+
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+from sklearn import tree
+from sklearn.ensemble import BaggingClassifier, RandomForestClassifier
+
+import graphviz
